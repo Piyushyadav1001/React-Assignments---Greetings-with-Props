324618c5ff57974288c4719a5af6ef7da244df11
Piyushyadav1001 <72632167+Piyushyadav1001@users.noreply.github.com>
2020-11-09 18:09:26 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,8 @@
 import React from "react";
-import Welcome from './components/Welcome';
+import Welcome from "./components/Welcome";
 
 const App = () => {
-  return (
-    <div>
-    <Welcome name="" />
-  <h2>Welcome to Newton School.</h2>
-  </div>
-  )
-}
-
+  return <Welcome name="" />;
+};
 
 export default App;
