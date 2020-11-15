6aac744595be91bb1c3098bfc0d479061f0b7222
Piyushyadav1001 <72632167+Piyushyadav1001@users.noreply.github.com>
2020-11-16 01:07:22 +0530

    Changes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,8 @@
 import React from "react";
-import "./App.css";
 const Welcome = (props) => {
   return (
     <>
-      <header>
-        Hey !<h1>{props.name}</h1>
-      </header>
+      <h1>Hey !{props.name}</h1>
       <h2>Welcome to Newton School.</h2>
     </>
   );
